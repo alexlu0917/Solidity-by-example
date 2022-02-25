1feea45f27094e084a36dc3c0e71c2f56fe1256b
alexlu0917 <alexlu0917.dev@gmail.com>
2022-02-25 02:22:35 +0000

    add the test for signature

--- VerifyingSignature/test/verifying_signature.js
+++ VerifyingSignature/test/verifying_signature.js
@@ -6,16 +6,18 @@ const VerifyingSignature = artifacts.require("VerifyingSignature");
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 contract("VerifyingSignature", function (accounts) {
-  it("should assert true", function () {
-    VerifyingSignature.deployed().then(instance => {
-      let sender = accounts[0];
-      let receiver = accounts[1];
-      console.log(sender, 'sender', receiver)
-      let message = web3.utils.soliditySha3(receiver, 10, 'Hello', 10);
-      console.log(message, 'message')
-      web3.eth.personal.sign(message, sender).then(res => {
-        expect(instance.contract.methods.verify(sender, receiver, 10, 'Hello', 10, res).call()).to.equal(true);
-      })
-    })
+  it("should assert true", async function () {
+    let verifyContract = await VerifyingSignature.deployed();
+    let sender = accounts[0];
+    let receiver = accounts[1];
+    console.log(sender, "sender", receiver);
+    let message = web3.utils.soliditySha3(receiver, 10, "Hello", 10);
+    console.log(message, "message", web3.eth.personal.sign);
+    let signature = await web3.eth.personal.sign(message, sender);
+    expect(
+      await verifyContract.contract.methods
+        .verify(sender, receiver, 10, "Hello", 10, signature)
+        .call()
+    ).to.equal(true);
   });
 });
